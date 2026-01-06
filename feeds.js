df718e44149f8f97967a49d8346939494fd84470
Mr. X <ximdotro@gmail.com>
2026-01-06 21:03:52 +0200

    Update feeds.js

--- feeds.js
+++ feeds.js
@@ -46,8 +46,8 @@ function get_marketplaces() {
   marketplaces.push({'name': 'count_monerica', 'feed': 'https://monerica.com', 'format': 'scraper'});
   marketplaces.push({'name': 'bitejo', 'feed': 'https://xmrbazaar.com/rss', 'format': 'rss'});
   marketplaces.push({'name': 'reddit_monero_market', 'feed': 'https://www.reddit.com/r/moneromarket.rss', 'format': 'atom'});
-  marketplaces.push({'name': 'twitter_monero', 'feed': 'https://nitter.privacydev.net/monero/rss', 'format': 'rss'}); // UPDATED Nitter link
-  marketplaces.push({'name': 'telegram_monero_market', 'feed': 'https://nitter.privacydev.net/monero_market/rss', 'format': 'rss'}); // UPDATED Nitter link
+  marketplaces.push({'name': 'twitter_monero', 'feed': 'https://nitter.poast.org/monero/rss', 'format': 'rss'}); // UPDATED Nitter link
+  marketplaces.push({'name': 'telegram_monero_market', 'feed': 'https://nitter.poast.org/monero_market/rss', 'format': 'rss'}); // UPDATED Nitter link
   marketplaces.push({'name': 'reddit_monero', 'feed': 'https://www.reddit.com/r/monero.rss', 'format': 'atom'});
   return marketplaces;
 }
